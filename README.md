c1f199140364cd6ff8e364c0dccf58731ae19849
breinerpico <breinerviviescas@colegioguanenta.edu.co>
2024-03-21 17:40:45 -0500

    se realizo el programa

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # instrucciones-repetitivas-
 cree un programa que calcule la suma de ciertos números
+
+# DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
+# CONSTRUCCION
---
+++ instrucciones-repetitivas.py
@@ -0,0 +1,18 @@
+import math
+
+#input 
+
+N= int(input("Digite el valor de n: "))
+
+#processing
+
+suma = 0
+i = 1
+
+while (i <= N):
+    suma =suma + i
+    i = 1
+
+#output
+
+print("La suma es: " + str(suma))
